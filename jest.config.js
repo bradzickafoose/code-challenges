43b1e9c8fda2bc599126b4550147806f3723ee7e
Brad <brad@viziworx.com>
2020-05-08 20:29:12 -0400

    adds jest configuration

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // Displays the name of the repository beside each test
+  displayName: "Code Challenges",
+
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: null,
+
+  // Allows for custom setup configurations
+  setupFilesAfterEnv: ["jest-extended"]
+};
